887bdda54893008f569ab07b0d30666628315ae9
Zilu (Peter) Tang <zilu.p.tang@gmail.com>
2026-05-11 15:18:31 -0400

    Update run_codellama_load_altform_the_iter.sh

--- state_probing_experiments/scripts/mention_probing/run_codellama_load_altform_the_iter.sh
+++ state_probing_experiments/scripts/mention_probing/run_codellama_load_altform_the_iter.sh
@@ -3,7 +3,9 @@
 module load miniconda
 module load cuda/11.8
 
-conda activate box-models
+conda activate <your_env>
+export HF_HOME="<your hf home>"
+
 
 for layer in $(seq 1 41);
 do 
@@ -11,16 +13,16 @@ do
     python train_probe_LLM.py \
         --model_type llama \
         --exp_name mention \
-        --dataset_path $ROOT/../data/boxes_altAlways_default_maxop12_5k \
-        --model_path /projectnb/tin-lab/sebastian/box-models/checkpoints/CodeLlama-13b-hf \
+        --dataset_path data/boxes_altAlways_default_maxop12_5k \
+        --model_path codellama/CodeLlama-13b-hf \
         --layer $layer \
         --epo 64 \
         --binary_probe \
         --exclude_empty \
         --condition_on the \
-        --checkpoint_root probe_checkpoints/codellama-13b/mention_the \
+        --checkpoint_root probe_experiments/probe_checkpoints/codellama-13b/mention_the \
         --load_model_representation \
-        --model_representation_path representations/codellama-13b/exclude_empty_conditioned_on_the \
+        --model_representation_path probe_experiments/representations/codellama-13b/exclude_empty_conditioned_on_the \
         --subsample  \
-        --object_vocabulary_file data/llama_friendly_objects.csv \
+        --object_vocabulary_file data/objects/llama_friendly_objects.csv \
 done
