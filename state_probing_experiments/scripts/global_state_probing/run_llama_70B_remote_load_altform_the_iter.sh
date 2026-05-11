d65d39daa846a22b6b6ba031c2325f6eeebc693c
Zilu (Peter) Tang <zilu.p.tang@gmail.com>
2026-05-11 14:56:36 -0400

    Update run_llama_70B_remote_load_altform_the_iter.sh

--- state_probing_experiments/scripts/global_state_probing/run_llama_70B_remote_load_altform_the_iter.sh
+++ state_probing_experiments/scripts/global_state_probing/run_llama_70B_remote_load_altform_the_iter.sh
@@ -10,7 +10,7 @@ do
         --model_type llama \
         --exp_name global \
         --dataset_path $ROOT/../data/boxes_altAlways_default_maxop12_5k \
-        --model_path meta-llama/Llama-3.1-70B\
+        --model_path meta-llama/Meta-Llama-3.1-70B\
         --layer $layer \
         --epo 64 \
         --condition_on the \
