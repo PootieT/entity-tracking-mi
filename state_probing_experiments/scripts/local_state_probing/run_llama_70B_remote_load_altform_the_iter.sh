6a71008ee7d30cbdc037a6043937fda5dc8d4075
Zilu (Peter) Tang <zilu.p.tang@gmail.com>
2026-05-11 15:17:03 -0400

    Update run_llama_70B_remote_load_altform_the_iter.sh

--- state_probing_experiments/scripts/local_state_probing/run_llama_70B_remote_load_altform_the_iter.sh
+++ state_probing_experiments/scripts/local_state_probing/run_llama_70B_remote_load_altform_the_iter.sh
@@ -1,7 +1,8 @@
 module load miniconda
 module load cuda/11.8
 
-conda activate box-models
+conda activate <your_env>
+export HF_HOME="<your hf home>"
 
 for layer in $(seq 1 81);
 do
@@ -9,18 +10,18 @@ do
     python train_probe_LLM.py \
         --model_type llama \
         --exp_name binary \
-        --dataset_path $ROOT/../data/boxes_altAlways_default_maxop12_5k \
+        --dataset_path data/boxes_altAlways_default_maxop12_5k \
         --model_path meta-llama/Meta-Llama-3.1-70B\
         --layer $layer \
         --epo 64 \
         --binary \
         --exclude_empty \
         --condition_on the \
-        --checkpoint_root probe_checkpoints/llama3-70b/binary_probing \
+        --checkpoint_root probe_experiments/probe_checkpoints/llama3-70b/binary_probing \
         --load_model_representation \
-        --model_representation_path representations/llama3-70b/exclude_empty_conditioned_on_the \
+        --model_representation_path probe_experiments/representations/llama3-70b/exclude_empty_conditioned_on_the \
         --subsample \
         --remote \
         --act_batch_size 16 \
-        --object_vocabulary_file data/llama_friendly_objects.csv \
+        --object_vocabulary_file data/objects/llama_friendly_objects.csv \
 done
