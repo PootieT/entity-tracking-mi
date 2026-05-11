32f3cc480254853d2e6733de4723319deb4f784d
Zilu (Peter) Tang <zilu.p.tang@gmail.com>
2026-05-11 15:09:49 -0400

    Update paths and environment variables in script

--- state_probing_experiments/scripts/global_state_probing/run_llama_70B_remote_save_altform_the.sh
+++ state_probing_experiments/scripts/global_state_probing/run_llama_70B_remote_save_altform_the.sh
@@ -3,22 +3,23 @@
 module load miniconda
 module load cuda/11.8
 
-conda activate box-models
+conda activate <your_env>
+export HF_HOME="<your hf home>"
 
 echo "Probing layer ${1}"
 
 python train_probe_LLM.py \
     --model_type llama \
     --exp_name global \
-    --dataset_path $ROOT/../data/boxes_altAlways_default_maxop12_5k \
+    --dataset_path data/boxes_altAlways_default_maxop12_5k \
     --model_path meta-llama/Meta-Llama-3.1-70B\
     --layer $layer \
     --epo 64 \
     --condition_on the \
-    --checkpoint_root probe_checkpoints/llama3-70b/global_probing \
+    --checkpoint_root probe_experiments/probe_checkpoints/llama3-70b/global_probing \
     --save_model_representation \
-    --model_representation_path representations/llama3-70b/include_empty_conditioned_on_the \
+    --model_representation_path probe_experiments/representations/llama3-70b/include_empty_conditioned_on_the \
     --subsample \
     --remote \
     --act_batch_size 16 \
-    --object_vocabulary_file data/llama_friendly_objects.csv \
+    --object_vocabulary_file data/objects/llama_friendly_objects.csv \
