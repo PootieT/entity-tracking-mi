ca9657a996868e4156cb2aaa8e0d474ded0ef4d2
Zilu (Peter) Tang <zilu.p.tang@gmail.com>
2026-05-11 15:16:11 -0400

    Update paths and environment variables in script

--- state_probing_experiments/scripts/local_state_probing/run_codellama_save_altform_the.sh
+++ state_probing_experiments/scripts/local_state_probing/run_codellama_save_altform_the.sh
@@ -3,21 +3,22 @@
 module load miniconda
 module load cuda/11.8
 
-conda activate box-models
+conda activate <your_env>
+export HF_HOME="<your hf home>"
 
 echo "Probing layer ${1}"
 
 python train_probe_LLM.py \
     --model_type llama \
     --exp_name binary \
-    --dataset_path $ROOT/../data/boxes_altAlways_default_maxop12_5k \
-    --model_path /projectnb/tin-lab/sebastian/box-models/checkpoints/CodeLlama-13b-hf \
+    --dataset_path data/boxes_altAlways_default_maxop12_5k \
+    --model_path codellama/CodeLlama-13b-hf \
     --layer $1 \
     --epo 64 \
     --binary_probe \
     --exclude_empty \
     --condition_on the \
-    --checkpoint_root probe_checkpoints/codellama-13b/binary_the \
+    --checkpoint_root probe_experiments/probe_checkpoints/codellama-13b/binary_the \
     --save_model_representation \
-    --model_representation_path representations/codellama-13b/exclude_empty_conditioned_on_the \
+    --model_representation_path probe_experiments/representations/codellama-13b/exclude_empty_conditioned_on_the \
     --subsample 
