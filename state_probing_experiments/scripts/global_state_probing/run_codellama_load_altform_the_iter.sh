fc48d710d9cae4fbab8a213e9f31ad5f3536c280
Zilu (Peter) Tang <zilu.p.tang@gmail.com>
2026-05-11 15:01:44 -0400

    Update dataset and checkpoint paths in script

--- state_probing_experiments/scripts/global_state_probing/run_codellama_load_altform_the_iter.sh
+++ state_probing_experiments/scripts/global_state_probing/run_codellama_load_altform_the_iter.sh
@@ -1,5 +1,4 @@
 
-
 module load miniconda
 module load cuda/11.8
 
@@ -11,15 +10,15 @@ do
     python train_probe_LLM.py \
         --model_type llama \
         --exp_name global \
-        --dataset_path $ROOT/../data/boxes_altAlways_default_maxop12_5k \
+        --dataset_path data/boxes_altAlways_default_maxop12_5k \
         --model_path codellama/CodeLlama-13b-hf \
         --layer $layer \
         --epo 64 \
         --condition_on the \
-        --checkpoint_root probe_checkpoints/codellama-13b/global_the \
+        --checkpoint_root probe_experiments/probe_checkpoints/codellama-13b/global_the \
         --load_model_representation \
-        --model_representation_path representations/codellama-13b/include_empty_conditioned_on_the \
+        --model_representation_path probe_experiments/representations/codellama-13b/include_empty_conditioned_on_the \
         --subsample  \
-        --object_vocabulary_file data/llama_friendly_objects.csv \
+        --object_vocabulary_file data/objects/llama_friendly_objects.csv \
         
 done
