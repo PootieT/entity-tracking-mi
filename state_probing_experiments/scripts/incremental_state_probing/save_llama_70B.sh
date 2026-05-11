47c26b817daf795ea2587462bbeb4ad586f0ebe4
Zilu (Peter) Tang <zilu.p.tang@gmail.com>
2026-05-11 15:11:23 -0400

    Update save_llama_70B.sh

--- state_probing_experiments/scripts/incremental_state_probing/save_llama_70B.sh
+++ state_probing_experiments/scripts/incremental_state_probing/save_llama_70B.sh
@@ -2,23 +2,25 @@
 module load miniconda
 module load cuda/11.8
 
-conda activate box-models
+conda activate <your_env>
+export HF_HOME="<your hf home>"
+
 
 echo "Probing layer ${1}"
 
 python train_probe_LLM.py \
     --model_type llama \
     --exp_name incremental_local_state \
-    --dataset_path /projectnb/tin-lab/qazhao/ICML26/entity-tracking-gemma/data/boxes_altAlways_default_maxop12_5k \
+    --dataset_path data/boxes_altAlways_default_maxop12_5k \
     --model_path meta-llama/Meta-Llama-3.1-70B\
     --layer $1 \
     --epo 64 \
     --binary \
     --condition_on number \
-    --checkpoint_root probe_checkpoints/llama3-70b/altform/incremental_local_state \
+    --checkpoint_root probe_experiments/probe_checkpoints/llama3-70b/altform/incremental_local_state \
     --save_model_representation \
-    --model_representation_path representations/llama3-70b/ICL_Free/Altform/incremental_local_state \
+    --model_representation_path probe_experiments/representations/llama3-70b/ICL_Free/Altform/incremental_local_state \
     --subsample \
     --remote \
     --save_activations_ckpts \
-    --object_vocabulary_file /projectnb/tin-lab/qazhao/ICML26/entity-tracking-gemma/data/objects/llama_friendly_objects.csv \
+    --object_vocabulary_file data/objects/llama_friendly_objects.csv \
