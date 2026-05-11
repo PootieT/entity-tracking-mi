e04f0e63aff330b5a62ec2b3ed8071faaa9bc6bb
Zilu (Peter) Tang <zilu.p.tang@gmail.com>
2026-05-11 15:02:52 -0400

    Update dataset and checkpoint paths in script

--- state_probing_experiments/scripts/global_state_probing/run_codellama_save_altform_the.sh
+++ state_probing_experiments/scripts/global_state_probing/run_codellama_save_altform_the.sh
@@ -10,12 +10,12 @@ echo "Probing layer ${1}"
 python train_probe_LLM.py \
     --model_type llama \
     --exp_name global \
-    --dataset_path $ROOT/../data/boxes_altAlways_default_maxop12_5k \
+    --dataset_path data/boxes_altAlways_default_maxop12_5k \
     --model_path codellama/CodeLlama-13b-hf \
     --layer $1 \
     --epo 64 \
     --condition_on the \
-    --checkpoint_root probe_checkpoints/codellama-13b/global_the \
+    --checkpoint_root probe_experiments/probe_checkpoints/codellama-13b/global_the \
     --save_model_representation \
-    --model_representation_path representations/codellama-13b/include_empty_conditioned_on_the \
+    --model_representation_path probe_experiments/representations/codellama-13b/include_empty_conditioned_on_the \
     --subsample 
